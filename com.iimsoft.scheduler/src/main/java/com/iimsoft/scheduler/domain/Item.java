2f317c2b74df7fb5ea307748ae164d721b374c1c
cc <ch21237@qq.com>
2025-10-27 09:27:59 +0800

    物料增加提前时间

--- com.iimsoft.scheduler/src/main/java/com/iimsoft/scheduler/domain/Item.java
+++ com.iimsoft.scheduler/src/main/java/com/iimsoft/scheduler/domain/Item.java
@@ -5,20 +5,28 @@ import java.util.Objects;
 public class Item {
     private String code;
     private String name;
+    private int leadTime; // 单位：天
 
     public Item() {
     }
 
     public Item(String code, String name) {
+        this(code, name, 0);
+    }
+
+    public Item(String code, String name, int leadTime) {
         this.code = code;
         this.name = name;
+        this.leadTime = leadTime;
     }
 
     public String getCode() { return code; }
     public String getName() { return name; }
+    public int getLeadTime() { return leadTime; }
 
     public void setCode(String code) { this.code = code; }
     public void setName(String name) { this.name = name; }
+    public void setLeadTime(int leadTime) { this.leadTime = leadTime; }
 
     @Override
     public boolean equals(Object o) {
